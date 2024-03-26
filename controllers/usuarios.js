815920b485a9f924cfe27409a4e57dec3d52a61f
Raul Campos <raulo_974@hotmail.com>
2024-03-25 21:00:51 -0500

    Update al renew token endpoint

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -21,20 +21,21 @@ const getUsuarios = async (req, res) => {
 };
 
 const crearUsuarios = async (req, res = response) => {
-  const { email, password, nombre } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const existeEmail = Usuario.findOne({ email });
+    const existeEmail = await Usuario.findOne({ email });
 
-    if (!existeEmail.findOne()) {
+    if (existeEmail) {
       return res.status(400).json({
         ok: false,
-        msg: "El correo ya esta registrado",
+        msg: "El correo ya está registrado",
       });
     }
 
     const usuario = new Usuario(req.body);
-    //Encriptar password
+
+    // Encriptar contraseña
     const salt = bcrypt.genSaltSync();
     usuario.password = bcrypt.hashSync(password, salt);
 
@@ -53,7 +54,7 @@ const crearUsuarios = async (req, res = response) => {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: "Error inesperado",
+      msg: "Error inesperado... revisar logs",
     });
   }
 };
